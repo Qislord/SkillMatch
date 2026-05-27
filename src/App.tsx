7ffe6533db300ba5056bab5c4e488d60d6d7b687
Qislord <AnanadKryt@gmail.com>
2026-05-27 20:33:26 +0700

    accept incoming changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-<<<<<<< Updated upstream
-import './App.css'
-=======
 import Header from '../components/header.tsx'
 import Search from '../components/search.tsx'
->>>>>>> Stashed changes
 
 function App() {
 
   return (
-<<<<<<< Updated upstream
-    <></>
-=======
     <>
       <Header />
       <Search></Search>
     </>
->>>>>>> Stashed changes
   )
 }
 
